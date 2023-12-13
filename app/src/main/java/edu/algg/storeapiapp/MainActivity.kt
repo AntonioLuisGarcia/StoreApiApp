5d7cf097c73b45a7c4cb342e5d77308e380145c8
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-12-13 12:31:24 +0100

    Barra de navegación

--- app/src/main/java/edu/algg/storeapiapp/MainActivity.kt
+++ app/src/main/java/edu/algg/storeapiapp/MainActivity.kt
@@ -2,14 +2,33 @@ package edu.algg.storeapiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import edu.algg.storeapiapp.databinding.ActivityMainBinding
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_main_area) as NavHostFragment
+        navController = navHostFragment.navController
+        //val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.productListFragment,
+                R.id.productListFragment
+            )
+        )
+        navView.setupWithNavController(navController)
     }
 }
